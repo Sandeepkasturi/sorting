f618ae8424b79a4a883d404e2b808513650b8e91
Sandeepkasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-02-07 21:40:20 +0530

    code

---
+++ sorting.js
@@ -0,0 +1,13 @@
+var num = [12, 25, 31, 23, 75, 81, 100];
+         
+    // Print Before Sorting Array
+    console.log("Original Array");
+    console.log(num);
+         
+    // Call sort function
+    num.sort();
+     
+    console.log("After Sorting in Ascending Order");
+         
+    // Print Sorted Numeric Array
+    console.log(num);   
